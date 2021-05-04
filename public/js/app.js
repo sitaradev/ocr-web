76b6185d0d21eef59928d8e2252890625ac9dc29
Aditya-011 <adihgr18@gmail.com>
2021-05-04 22:24:38 +0530

    upload button styled

--- public/js/app.js
+++ public/js/app.js
@@ -88,3 +88,25 @@ function loader() {
     }, 3000);
   }
 }
+var buttons = document.getElementsByTagName("button");
+
+Array.prototype.forEach.call(buttons, function (b) {
+  b.addEventListener("click", createRipple);
+});
+
+function createRipple(e) {
+  if (this.getElementsByClassName("ripple").length > 0) {
+    this.removeChild(this.childNodes[1]);
+  }
+
+  var circle = document.createElement("div");
+  this.appendChild(circle);
+
+  var d = Math.max(this.clientWidth, this.clientHeight);
+  circle.style.width = circle.style.height = d + "px";
+
+  circle.style.left = e.clientX - this.offsetLeft - d / 2 + "px";
+  circle.style.top = e.clientY - this.offsetTop - d / 2 + "px";
+
+  circle.classList.add("ripple");
+}
