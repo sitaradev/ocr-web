2666ff40d9614f8947a4d2826468cad0342ed0d3
sitaradev <120164511+sitaradev@users.noreply.github.com>
2023-08-25 12:10:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 // const fsPromises = require("fs").promises;
-// var result;
-// const directory = "images/";
-// const { v4: uuidv4 } = require("uuid");
-// const fsExtra = require("fs-extra");
-// const multer = require("multer");
-// const fs = require("fs");
-// const mindee = require("mindee");
-// const bodyParser = require("body-parser");
+var result;
+const directory = "images/";
+const { v4: uuidv4 } = require("uuid");
+const fsExtra = require("fs-extra");
+const multer = require("multer");
+const fs = require("fs");
+const mindee = require("mindee");
+const bodyParser = require("body-parser");
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 
