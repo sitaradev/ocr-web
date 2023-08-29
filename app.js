ee96d881f50a5c743eac2e0ccfcad9d62f99e17a
sitaradev <120164511+sitaradev@users.noreply.github.com>
2023-08-29 19:07:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const mindee = require("mindee");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-app.set('views', path.join(__dirname, "views"));
-app.set('view engine','ejs'); 
-app.engine('ejs', require('ejs').__express);
-app.set("public", path.join(__dirname, "public"));
+// app.set('views', path.join(__dirname, "views"));
+// app.set('view engine','ejs'); 
+// app.engine('ejs', require('ejs').__express);
+// app.set("public", path.join(__dirname, "public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // all different api keys----
 // const ALL_KEYS = {
