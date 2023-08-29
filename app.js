c671911fb920d8e7cabc7d566ee91808b77585aa
= <=>
2023-08-29 18:06:57 +0530

    set public

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const path = require("path");
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine','ejs'); 
 app.engine('ejs', require('ejs').__express);
-server.set("public", path.join(__dirname, "public"));
+app.set("public", path.join(__dirname, "public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
